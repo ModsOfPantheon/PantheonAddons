2d8582a3e4eb92ec11447ab25c79e6cda788b52f
owisx <jasonmwegner@gmail.com>
2025-03-05 19:21:48 -0600

    Remove XP text from bar when disabling.  Readd current xp when reenabling

--- PantheonAddons/EnhancedExperienceBar/EnhancedExperienceBar.cs
+++ PantheonAddons/EnhancedExperienceBar/EnhancedExperienceBar.cs
@@ -12,6 +12,7 @@ public sealed class EnhancedExperienceBar : Addon
     private float _originalWidth;
     private IAddonWindow? _xpWindow;
     private IAddonTextComponent? _xpText;
+    private IPlayer? _player;
 
     public override void OnCreate()
     {
@@ -24,12 +25,20 @@ public sealed class EnhancedExperienceBar : Addon
     {
         _xpWindow?.SetHeight(_xpWindow.Height + 10);
         _xpWindow?.SetWidth(_xpWindow.Width / 2);
+        var experience = _player?.GetExperience();
+
+        if (experience == null)
+        {
+            return;
+        }
+        _xpText?.SetText(CreateText(experience));
     }
-    
+
     public override void Disable()
     {
         _xpWindow?.SetHeight(_originalHeight);
         _xpWindow?.SetWidth(_originalWidth);
+        _xpText?.SetText("");
     }
 
     public override IEnumerable<IConfigurationValue> GetConfiguration()
@@ -42,14 +51,16 @@ public sealed class EnhancedExperienceBar : Addon
         WindowPanelEvents.OnExperienceBarReady.Unsubscribe(OnExperienceBarReady);
         LocalPlayerEvents.OnExperienceChanged.Unsubscribe(OnExperienceChanged);
     }
- 
+
     private void OnPlayerEntered(IPlayer player)
     {
         if (!player.IsLocalPlayer)
         {
             return;
         }
-        
+
+        _player = player;
+
         var experience = player.GetExperience();
 
         if (experience == null)
@@ -69,14 +80,14 @@ public sealed class EnhancedExperienceBar : Addon
         _originalHeight = window.Height;
         _originalWidth = window.Width;
         _xpWindow = window;
-        
+
         _xpWindow.SetHeight(_originalHeight + 10);
         _xpWindow.SetWidth(_originalWidth / 2);
 
         _xpText = _xpWindow.AddTextComponent("0 / 0");
         _xpText.SetSize(500, 20);
     }
-    
+
     private static string CreateText(PlayerExperience playerExperience)
     {
         return $"{playerExperience.Current:N0} / {playerExperience.ToNextLevel:N0} ({playerExperience.ExperiencePercentage * 100:F}%)";
