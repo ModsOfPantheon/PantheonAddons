ef263f0c5c2b7664dbbe9a9d28bb05e7c1bdc578
ModsOfPantheon <ModsOfPantheon@users.noreply.github.com>
2025-03-08 22:25:59 +0000

    Add set background and slider colour to xp bar

--- PantheonAddonFramework/UI/IXpBarWindow.cs
+++ PantheonAddonFramework/UI/IXpBarWindow.cs
@@ -3,4 +3,6 @@ namespace PantheonAddonFramework.UI;
 public interface IXpBarWindow : IAddonWindow
 {
     void ShowTicks(bool show);
+    void SetBackgroundColour(float red, float green, float blue, float alpha);
+    void SetSliderColour(float red, float green, float blue, float alpha);
 }
