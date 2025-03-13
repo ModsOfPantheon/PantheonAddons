271ed63533ed68053cf0d9e6789d0244f7df3bf5
owisx <jasonmwegner@gmail.com>
2025-03-12 22:18:25 -0500

    Update wording

--- PantheonAddonFramework/UI/IAddonTextComponent.cs
+++ PantheonAddonFramework/UI/IAddonTextComponent.cs
@@ -10,7 +10,7 @@ public interface IAddonTextComponent
     void SetFontColor(byte red, byte green, byte blue, byte alpha);
     void SetFontColor(float red, float green, float blue, float alpha);
     /// <summary>
-    /// Set TextComponent font to provided font name, for example to use Impact.ttf, call function with "Impact"
+    /// Set TextComponent font to provided font name, for example to use Impact.ttf, call method with "Impact"
     /// </summary>
     /// <param name="font">Filename for font without the .ttf extension</param>
     void SetFont(string font);
